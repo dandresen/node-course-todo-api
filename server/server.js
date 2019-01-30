943e0d118fd792bb9c79b7cbba7e1be756d7fb39
dandresen <dandresen01@gmail.com>
2019-01-29 20:24:17 -0600

    added GET /users/ and auth function

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.patch('/todos/:id', (req, res) => {
 
 
 
-// Users 
+// POST Users 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -112,6 +112,19 @@ app.post('/users', (req, res) => {
       res.status(400).send(e);
     })
   });
+
+app.get('/users/me', (req, res) => {
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
+});
   
 
 
